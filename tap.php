4bc3dfe5416e04cdfa091f8ee98fa88bfba869ed
Tobias Thaden <hello@tobiasthaden.com>
2020-04-11 16:57:30 +0200

    Add docblock

--- tap.php
+++ tap.php
@@ -1,6 +1,13 @@
 <?php
 
 if (! function_exists('tap')) {
+    /**
+     * Call the given callback with the given value then return the value.
+     *
+     * @param  mixed  $value
+     * @param  callable  $callable
+     * @return mixed
+     */
     function tap($value, $callable)
     {
         $callable($value);
